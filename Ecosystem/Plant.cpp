1688557c467f53b8b96b4ff05dbcb0b5e3370340
Klimentiy Mandra <56902939+klimkam@users.noreply.github.com>
2024-05-09 13:16:05 -0400

    Added Rabbits Sprites

--- Ecosystem/Plant.cpp
+++ Ecosystem/Plant.cpp
@@ -10,6 +10,6 @@ void Plant::Start()
 {
 	m_currentSprite = GetRandomValue(0, (*m_atlas).width/ m_textureWidth);
 
-	m_positionX = GetRandomValue(0 + m_textureWidth, GetScreenWidth() - m_textureWidth);
-	m_positionY = GetRandomValue(0 + m_textureWidth, GetScreenHeight() - m_textureWidth);
+	m_positionX = GetRandomValue(m_textureWidth, GetScreenWidth() - m_textureWidth);
+	m_positionY = GetRandomValue(m_textureWidth, GetScreenHeight() - m_textureWidth);
 }
